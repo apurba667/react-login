27ff45bcabed0f2ae0fb7cfe53e966e664c00cdb
APURBA ROY <apurbaanikroy@gmail.com>
2021-03-13 00:57:16 +0600

    ...

---
+++ src/fireBaseConfiq.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDhz2Ze8znu35KzMU3geH3LotYFAT0r-l8",
+    authDomain: "ama-john-simple-86347.firebaseapp.com",
+    projectId: "ama-john-simple-86347",
+    storageBucket: "ama-john-simple-86347.appspot.com",
+    messagingSenderId: "506766531789",
+    appId: "1:506766531789:web:4a9483e680f1defa5a9d84"
+  };
+  export default firebaseConfig;
